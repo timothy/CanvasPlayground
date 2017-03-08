5683fd1e828e699f9ec055cc3b640c194f5d7738
Timothy <tabradford@liberty.edu>
2017-03-08 14:52:56 -0500

    Added Line through graph

--- app/view5/view5.js
+++ app/view5/view5.js
@@ -14,39 +14,66 @@ angular.module('myApp.' + moduleName, ['ngRoute'])
     }])
 
     .controller(moduleName + 'Ctrl', [function () {
-        var canvas, ctx, scores, width, curBarHeight, curX, curY, spacing, base, i, l;
+        var canvas, ctx, scores, width, curBarHeight, curX, curY, startX = 0, startY = 0, curLineX,cirLineWidth = 5, spacing, base, i, l;
 
         canvas = $('#testCanvas3')[0];//document.getElementById('testCanvas');
         ctx = canvas.getContext('2d');
 
-        scores = [70, 52, 86, 100, 57, 62, 98];
+        scores = [70, 52, 86, 100, 57, 62, 98, 5, 43, 20, 99, 110];
         width = 50;
         curX = 50;
         spacing = 2;
-        base = 200;
 
         ctx.fillStyle = '#d8838e';
 
+        startX = curX + spacing + width / 2;
+        curLineX = cirLineWidth +curX + (width / 2);
         l = scores.length;
-
         for (i = 0; i < l; i++) {
             curBarHeight = scores[i];
             curY = canvas.height - curBarHeight;
-            ctx.fillStyle = i === 4 ? '#ff7272' : '#d8838e';
-            ctx.fillRect(curX, curY, width, scores[i]);
 
 
-            ctx.fillStyle = i === 4 ? '#00AE82' : '#D84848';
-            ctx.beginPath();
-            ctx.arc(curX + width / 2, curY, 10, 0, 2 * Math.PI, false);
-            ctx.fill();
-            ctx.strokeStyle = "pink";
-            ctx.lineWidth = 2;
-            ctx.stroke();
+            drawRect();
+            drawLine();
+            drawCircle();
 
             curX += width + spacing;
+            curLineX = cirLineWidth +curX + (width / 2);
         }
 
+        function drawLine() {
+            if (startY === 0) startY = curY;
+            ctx.beginPath();
+            ctx.strokeStyle = "blue";
+            ctx.lineWidth = 3;
+            ctx.moveTo(startX, startY);
+            ctx.lineTo(curLineX - cirLineWidth - 10, curY);
+            ctx.stroke();
+            ctx.closePath();
+            startX = curLineX + spacing + cirLineWidth;
+            startY = curY;
+        }
+
+        function drawRect(){
+            //draw rectangle
+            ctx.fillStyle = i === 4 ? '#ff7272' : '#d8838e';
+            ctx.fillRect(curX, curY, width, scores[i]);
+        }
+
+
+        function drawCircle() {
+            //draw circle
+            ctx.fillStyle = i === 4 ? '#00AE82' : '#D84848';
+            ctx.beginPath();
+            ctx.arc(curX + width/2, curY, 10, 0, 2 * Math.PI, false);//make a circle
+            ctx.fill();// fill the circle with the fillStyle
+            ctx.strokeStyle = "pink";//create a boarder
+            ctx.lineWidth = cirLineWidth;
+            ctx.stroke();
+        }
+
+        console.log(ctx.lineWidth);
 
         console.log(ctx);
         console.log(canvas);
