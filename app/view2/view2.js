0a3a0ae1906426686909e85b300abfff011e5fd9
Timothy <tabradford@liberty.edu>
2017-03-10 08:56:43 -0500

    Added auto size adjustment. Started building new graph design. Added margin functionality.

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -11,19 +11,37 @@ angular.module('myApp.view2', ['ngRoute'])
 
     .controller('View2Ctrl', [function () {
         var canvas = $('#testCanvas')[0];//document.getElementById('testCanvas');
+        canvas.width = window.innerWidth;
         var ctx = canvas.getContext('2d');
 
+        var margin = { top: 10, left: 10, right: 10, bottom: 10 };
         var scores = [100, 90, 86, 35, 57, 62, 98];
         var width = 50;
-        var currX = 50;
+        var currX = margin.left;// start at margin boarder
         var spacing = 2;
+        var lineHeight = 10;
 
-        ctx.fillStyle = '#d8838e';
 
-        for (var i = 0; i < scores.length; i++) {
-            ctx.fillRect(currX, canvas.height - scores[i], width, scores[i]);
-            currX += width + spacing;
+        function renderLines() {
+            ctx.beginPath();
+            ctx.moveTo(currX, canvas.height - margin.bottom);
+            ctx.lineTo(currX, canvas.height - lineHeight - margin.bottom);
+            ctx.stroke();
         }
+
+        function renderBars() {
+            ctx.fillStyle = '#d8838e';
+            var i;
+            for (i = 0; i < scores.length; i++) {
+                ctx.fillRect(currX, (canvas.height - scores[i]) - margin.bottom - lineHeight, width, scores[i]);
+                renderLines();
+                currX += width + spacing;
+            }
+            renderLines();
+        }
+
+
+        renderBars();
 
 
         //(x,y,width, height)
