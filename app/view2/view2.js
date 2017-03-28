edebd91e74d2a834c0f7c4ec887b5a3c6ae87430
Timothy <tabradford@liberty.edu>
2017-03-28 13:57:45 -0400

    Changed logic to make graph easily reusable. Moved logic into a factory. Added protoType class to handle factory.

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,197 +9,258 @@ angular.module('myApp.view2', ['ngRoute'])
         });
     }])
 
-    .controller('View2Ctrl', ['usage', function (usage) {
-        //Init Canvas
-        var canvas = $('#testCanvas')[0];//document.getElementById('testCanvas');
-        canvas.width = window.innerWidth - 20;
-        canvas.height = 100;
-        var ctx = canvas.getContext('2d');//context
+    .controller('View2Ctrl', ['usage', 'graph', function (usage, graph) {
 
-        //Set Background
-        ctx.fillStyle = "#B1081D";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        //Render Graph
+        graph.getGraph('testCanvas', usage.coffeeShop[0].data, false).render();
 
-        //Try to make things look a smooth as possible
-        ctx.translate(0.5, 0.5);
-        ctx.imageSmoothingEnabled = false;
+    }]).factory('graph', graphFactory);
 
-        //style variables
-        var margin = {top: 5, left: 5, right: 10, bottom: 13};
-        var textStyle = "10px Open Sans";
+//Dependency Injection
+graphFactory.$inject = [];
 
-        //Create boundaries for content to stay within
-        var widthOffset = 10;
-        var maxCanvasHeight = canvas.height - (Number(margin.top) + Number(margin.bottom)),//Offset by 15
-            maxCanvasWidth = canvas.width - (margin.left + margin.right) - widthOffset;//Offset by widthOffset
+/**
+ * Angular Factory service
+ * @returns {{}} a new instance of the Graph class
+ */
+function graphFactory() {
+    var service = {};
+    service.getGraph = getGraph;
 
-        //Get data from the service
-        var curUsage = usage.coffeeShop[0].data;
+    return service;
+}
 
-        //Width of each bar needs to scale with canvas barWidth
-        var barWidth = Math.round(maxCanvasWidth / curUsage.length);
+/**
+ * Angular 1 Factory method
+ * @param canvas the ID tag name of the canvas element the graph will reside in
+ * @param data the data that will populate the graph
+ * @param style (optional) custom styles.
+ * @returns {Graph} a new instance of the Graph class
+ */
+function getGraph(canvas, data, style) {
+    return new Graph(canvas, data, style);
+}
 
-        // start at margin boarder
-        var currX = margin.left;
-        // The spacing of each bar in the graph
-        var spacing = 1;
-        //The height of the line that separates the time from the graph
-        var lineHeight = 3;
-        // this should only be used for loops... instead of reinitializing i for each loop this is done once.
-        var i = 0, l = 0;// i === increment, l === length
-        //Find the largest bar height in order to scale all bars to the canvas height.
-        var maxBarHeight = getMaxChartValue(curUsage);
-        // Offset for scaling bar height to canvas
-        var barHeightOffset = (maxCanvasHeight / maxBarHeight);
+/**
+ * Constructor
+ * @param canvas the ID tag name of the canvas element the graph will reside in
+ * @param data the data that will populate the graph
+ * @param style (optional) custom styles.
+ * @constructor
+ */
+function Graph(canvas, data, style) {
+    //To ovoid scoping issues
+    var self = this;
 
-        //Find the index of the element with the same hour as now.
-        var timeNow = new Date().getHours() * 100;
-        var curTimeIndex = curUsage.findIndex(function (element, index, array) {
-            return element.time >= timeNow;
-        });
+    /**
+     * This variables Need to be exposed. This allows consumer to customize if desired.
+     */
+    //Init Canvas
+    self.canvas = $('#' + canvas)[0];//document.getElementById(canvas);
+    self.canvas.width = window.innerWidth - 20;
+    self.canvas.height = 100;
+    self.ctx = self.canvas.getContext('2d');//context
 
-        /**
-         * This renders vertical lines at the (currx, Y) coordinates
-         */
-        function renderLines() {
-            ctx.strokeStyle = "#d8838e";
-            ctx.lineWidth = spacing;
-            l = curUsage.length;
-            currX = margin.left;
+    //Set Background
+    self.ctx.fillStyle = !!style.background ? style.background : "#B1081D";
+    self.ctx.fillRect(0, 0, self.canvas.width, self.canvas.height);
 
-            ctx.strokeStyle = "#ffffff";
+    //Try to make things look a smooth as possible
+    self.ctx.translate(0.5, 0.5);
+    self.ctx.imageSmoothingEnabled = false;
+
+    /**
+     * style variables
+     */
+    //Graph margins
+    self.margin = !!style.margin ? style.margin : {top: 5, left: 5, right: 10, bottom: 13};
+    //Text size and font
+    self.textStyle = !!style.textProperties ? style.textProperties : "10px Open Sans";
+    // The spacing of each bar in the graph
+    self.spacing = !!style.spacing ? style.spacing : 1;
+    //The height of the line that separates the time from the graph
+    self.lineHeight = !!style.lineHeight ? style.lineHeight : 3;
+
+    //Create boundaries for content to stay within
+    self.widthOffset = 10;
+    self.maxCanvasHeight = self.canvas.height - (Number(self.margin.top) + Number(self.margin.bottom));//Offset by 15
+    self.maxCanvasWidth = self.canvas.width - (self.margin.left + self.margin.right) - self.widthOffset;//Offset by widthOffset
+
+    //Set Graph data
+    self.graphData = data;
+
+    //Width of each bar needs to scale with canvas barWidth
+    self.barWidth = Math.round(self.maxCanvasWidth / self.graphData.length);
+
+    // start at margin boarder
+    self.currX = self.margin.left;
+
+    //Find the largest bar height in order to scale all bars to the canvas height.
+    self.maxBarHeight = self.getMaxChartValue(self.graphData);
+    // Offset for scaling bar height to canvas
+    self.barHeightOffset = (self.maxCanvasHeight / self.maxBarHeight);
+
+    //Find the index of the element with the same hour as now.
+    self.timeNow = new Date().getHours() * 100;
+    self.curTimeIndex = self.graphData.findIndex(function (element, index, array) {
+        return element.time >= self.timeNow;
+    });
+
+
+    self.data = data;
+}
+
+/**
+ * Method Definition
+ * This draws the individual pieces of the graph in the right order
+ */
+Graph.prototype.render = function () {
+    var self = this;
+
+    renderBars(self);
+    renderTimes(self);
+    renderLines(self);
+};
+
+/**
+ * This will format the time according to the graph specification.
+ * @param time {number} military time.
+ * @returns {string} Formatted time string.
+ */
+Graph.prototype.timeFormat = function timeFormat(time) {
+    //Make sure time is a number and not a string
+    time = Number(time);
+
+    if (time > 1259 && time < 2200) {
+        return (time - 1200).toString().substring(0, 1) + " PM"
+    }
+
+    if (time >= 2200) {
+        return (time - 1200).toString().substring(0, 2) + " PM"
+    }
+
+    if (time <= 1259 && time > 1159) {
+        return time.toString().substring(0, 2) + " PM"
+    }
+
+    if (time <= 1159 && time > 959) {
+        return time.toString().substring(0, 2) + " AM"
+    }
+
+    if (time <= 59) {
+        return "12 AM"
+    }
+
+    return time.toString().substring(0, 1) + " AM"
+};
+
+/**
+ *
+ * @param chartArray - the array for the current chart
+ * @returns {number} the max usage number
+ */
+Graph.prototype.getMaxChartValue = function (chartArray) {
+    var max = 0, cur = 0, l = chartArray.length, i;
+    for (i = 0; i < l; i++) {
+        cur = chartArray[i].usage;
+        if (cur > max) max = cur;
+    }
+    return max;
+};
+
+/**
+ * This renders the times that are shown below the graph
+ */
+function renderTimes(self) {
+    var currX = self.margin.left;
+    var txt = "", l = self.graphData.length, hOffset = 4, i;
+
+    self.ctx.fillStyle = '#ffffff';
+    self.ctx.font = self.textStyle;
+
+    self.ctx.fillText(self.timeFormat(self.graphData[0].time), currX, self.canvas.height - hOffset);
+
+    //Only want times every 4th position.
+    for (i = 0; i < l; i++) {
+        if (i % 4 === 0 && i !== 0 && i !== l) {
+            txt = self.timeFormat(self.graphData[i].time);
+            self.ctx.fillText(txt, currX - Math.floor(self.ctx.measureText(txt).width / 2), self.canvas.height - hOffset);
+        }
+        currX += self.barWidth + self.spacing;
+    }
+    self.ctx.fillText(self.timeFormat(self.graphData[l - 1].time), ((self.canvas.width - self.ctx.measureText(txt).width) - self.spacing) - self.widthOffset, self.canvas.height - hOffset);
+}
+
+
+/**
+ * This renders vertical lines at the (currx, Y) coordinates
+ */
+function renderLines(self) {
+    var i, l = self.graphData.length;// i === increment, l === length
+    var currX = self.margin.left;
+
+    self.ctx.strokeStyle = "#d8838e";
+    self.ctx.lineWidth = self.spacing;
+    self.ctx.strokeStyle = "#ffffff";
+    renderSingleLine();
+    self.ctx.strokeStyle = "#d8838e";
+    for (i = 0; i < l; i++) {
+        if (i % 4 === 0 && i !== 0 && i !== l) {
+            self.ctx.strokeStyle = "#ffffff";
             renderSingleLine();
-            ctx.strokeStyle = "#d8838e";
-            for (i = 0; i < l; i++) {
-                if (i % 4 === 0 && i !== 0 && i !== l) {
-                    ctx.strokeStyle = "#ffffff";
-                    renderSingleLine();
-                    ctx.strokeStyle = "#d8838e";
-                } else {
-                    renderSingleLine();
-                }
-                currX += barWidth + spacing;
-                console.log(canvas.height - lineHeight - margin.bottom +.5);
-            }
-            ctx.strokeStyle = "#ffffff";
+            self.ctx.strokeStyle = "#d8838e";
+        } else {
             renderSingleLine();
-
-            function renderSingleLine() {
-                ctx.beginPath();
-                ctx.moveTo(currX, canvas.height - margin.bottom);
-                ctx.lineTo(currX, canvas.height - lineHeight - margin.bottom);
-                ctx.stroke();
-            }
         }
+        currX += self.barWidth + self.spacing;
+    }
+    self.ctx.strokeStyle = "#ffffff";
+    renderSingleLine();
 
-        /**
-         * This renders the times that are shown below the graph
-         */
-        function renderTimes() {
-            ctx.fillStyle = '#ffffff';
-            currX = margin.left;
-            ctx.font = textStyle;
-            var txt = "", l = curUsage.length, hOffset = 4;
-            ctx.fillText(timeFormat(curUsage[0].time), currX, canvas.height- hOffset);
-            for (i = 0; i < l; i++) {
-                if (i % 4 === 0 && i !== 0 && i !== l) {
-                    txt = timeFormat(curUsage[i].time);
-                    ctx.fillText(txt, currX - Math.floor(ctx.measureText(txt).width / 2), canvas.height - hOffset);
-                }
-                currX += barWidth + spacing;
-            }
-            ctx.fillText(timeFormat(curUsage[l - 1].time), ((canvas.width - ctx.measureText(txt).width) - spacing) - widthOffset, canvas.height- hOffset);
+    /**
+     * Draw a single vertical line using current X,Y coordinates state.
+     */
+    function renderSingleLine() {
+        self.ctx.beginPath();
+        self.ctx.moveTo(currX, self.canvas.height - self.margin.bottom);
+        self.ctx.lineTo(currX, self.canvas.height - self.lineHeight - self.margin.bottom);
+        self.ctx.stroke();
+    }
+}
+
+/**
+ * This renders the graph bars scaled to fit the canvas window
+ */
+function renderBars(self) {
+    var currX = self.margin.left;
+    var l = self.graphData.length, barHight = 0, barY = 0, i;
+
+    self.ctx.fillStyle = '#d8838e';
+    for (i = 0; i < l; i++) {
+        barHight = self.graphData[i].usage * self.barHeightOffset;//tried to make this easier to read
+        barY = (self.canvas.height - barHight) - self.margin.bottom - self.lineHeight;//tried to make this easier to read
+
+        if (self.curTimeIndex === i) {
+            self.ctx.fillStyle = 'white';
+            self.ctx.fillRect(currX, barY, self.barWidth, barHight);
+            self.ctx.fillStyle = '#d8838e';
+        } else {
+            self.ctx.fillRect(currX, barY, self.barWidth, barHight);
         }
+        currX += self.barWidth + self.spacing;
+    }
+    renderLineConnectors(self);
+}
 
-        /**
-         * This renders one horizontal line to connect all vertical lines.
-         */
-        function renderLineConnectors() {
-            ctx.beginPath();
-            ctx.strokeStyle = "#d8838e";
-            ctx.lineWidth = spacing;
-            ctx.moveTo(margin.left, canvas.height - margin.bottom - lineHeight);
-            ctx.lineTo(currX, canvas.height - margin.bottom - lineHeight);
-            ctx.stroke();
-            ctx.closePath();
-        }
-
-        /**
-         * This renders the graph bars scaled to fit the canvas window
-         */
-        function renderBars() {
-            ctx.fillStyle = '#d8838e';
-            currX = margin.left;
-            var l = curUsage.length, barHight = 0, barY = 0;
-            for (i = 0; i < l; i++) {
-                barHight = curUsage[i].usage * barHeightOffset;//tried to make this easier to read
-                barY = (canvas.height - barHight) - margin.bottom - lineHeight;//tried to make this easier to read
-
-                if (curTimeIndex === i) {
-                    ctx.fillStyle = 'white';
-                    ctx.fillRect(currX, barY, barWidth, barHight);
-                    ctx.fillStyle = '#d8838e';
-                } else {
-                    ctx.fillRect(currX, barY, barWidth, barHight);
-                }
-                currX += barWidth + spacing;
-            }
-            renderLineConnectors();
-        }
-
-
-        /**
-         * This draws the individual pieces of the graph in the right order
-         */
-        !function renderAll() {
-            renderBars();
-            renderTimes();
-            renderLines();
-        }();
-
-
-        /**
-         *
-         * @param chartArray - the array for the current chart
-         * @returns {number} the max usage number
-         */
-        function getMaxChartValue(chartArray) {
-            var max = 0, cur = 0, l = chartArray.length;
-            for (i = 0; i < l; i++) {
-                cur = chartArray[i].usage;
-                if (cur > max) max = cur;
-            }
-            return max;
-        }
-
-        /**
-         * This will format the time according to the graph specification.
-         * @param time {number} military time.
-         * @returns {string} Formatted time string.
-         */
-        function timeFormat(time) {
-            time = Number(time);//it is really important that this be a number and not a string that holds a number...
-            if (time > 1259 && time < 2200) {
-                return (time - 1200).toString().substring(0, 1) + " PM"
-            }
-
-            if (time >= 2200) {
-                return (time - 1200).toString().substring(0, 2) + " PM"
-            }
-
-            if (time <= 1259 && time > 1159) {
-                return time.toString().substring(0, 2) + " PM"
-            }
-
-            if (time <= 1159 && time > 959) {
-                return time.toString().substring(0, 2) + " AM"
-            }
-
-            if (time <= 59) {
-                return "12 AM"
-            }
-
-            return time.toString().substring(0, 1) + " AM"
-        }
-
-    }]);
+/**
+ * This renders one horizontal line to connect all vertical lines.
+ */
+function renderLineConnectors(self) {
+    self.ctx.beginPath();
+    self.ctx.strokeStyle = "#d8838e";
+    self.ctx.lineWidth = self.spacing;
+    self.ctx.moveTo(self.margin.left, self.canvas.height - self.margin.bottom - self.lineHeight);
+    self.ctx.lineTo(self.currX, self.canvas.height - self.margin.bottom - self.lineHeight);
+    self.ctx.stroke();
+    self.ctx.closePath();
+}
